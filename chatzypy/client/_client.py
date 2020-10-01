8729c2942d19e0cef6d1db829e21f13e2b633ef3
Spooky <averagewizard13@gmail.com>
2020-10-01 00:01:21 -0600

    docstring improvements

--- chatzypy/client/_client.py
+++ chatzypy/client/_client.py
@@ -11,32 +11,34 @@ class Client():
 		"""Authenticate with Chatzy to get an API token.
 
 		Args:
-			email (str): Email associated with the account
-			password (str): Password associated with the account
-			force (bool): Whether to ignore saved bearer tokens
+			email (str): The email associated with the account.
+			password (str): The password associated with the account.
+			force (bool): Whether to ignore saved bearer tokens.
 		Returns:
-			Client
+			Client: self
 
 		"""
 		raise NotImplementedError
 
-	def join_room(self, room_id):
+	def join_room(self, room_id, wait=False, timeout=15):
 		"""Join a room identified by the room_id argument.
 
 		Args:
 			room_id (str): ID of the room to leave.
+			wait (bool): Whether to wait until room is joined.
 		Returns:
-			Room
+			Room: Room if wait flag set (when joined)
 
 		"""
 		raise NotImplementedError
+
 	def leave_room(self, room_id):
 		"""Leave a room identified by the room_id argument.
 
 		Args:
 			room_id (str): ID of the room to leave.
 		Returns:
-			Room
+			Room: Room
 
 		"""
 		raise NotImplementedError
