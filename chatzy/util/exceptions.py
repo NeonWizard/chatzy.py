e8524aabd2af2c8c445a7650341641b9d226761a
NeonWizard <averagewizard13@gmail.com>
2020-10-23 09:45:51 -0700

    Add more custom exceptions

--- chatzy/util/exceptions.py
+++ chatzy/util/exceptions.py
@@ -6,6 +6,18 @@ class NotFound(Exception):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
+class Forbidden(Exception):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
 class NotAuthenticated(Exception):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+class RateLimit(Exception):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+class InvalidContent(Exception):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
